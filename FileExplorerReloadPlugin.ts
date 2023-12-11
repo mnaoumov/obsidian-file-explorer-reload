f0e60bd253728840f2fca0e7abbaead34d90ac78
Michael Naumov <mnaoumov@gmail.com>
2023-12-11 10:32:13 -0600

    Add context menu commands to reload folder

--- FileExplorerReloadPlugin.ts
+++ FileExplorerReloadPlugin.ts
@@ -1,6 +1,8 @@
 import {
+    Menu,
     Plugin,
-    TFolder
+    TAbstractFile,
+    TFolder,
 } from "obsidian";
 
 const ROOT_PATH = "/";
@@ -12,13 +14,15 @@ export default class FileExplorerReloadPlugin extends Plugin {
             name: "Reload File Explorer",
             callback: this.reloadFileExplorer.bind(this)
         });
+
+        this.registerEvent(this.app.workspace.on("file-menu", this.handleFileMenu.bind(this)));
     }
 
     private async reloadFileExplorer(): Promise<void> {
-        await this.reloadDirectory(ROOT_PATH);
+        await this.reloadDirectory(ROOT_PATH, true);
     }
 
-    private async reloadDirectory(directoryPath: string): Promise<void> {
+    private async reloadDirectory(directoryPath: string, isRecursive: boolean): Promise<void> {
         const isRoot = directoryPath === ROOT_PATH;
         const adapter = this.app.vault.adapter;
         console.debug(`Reloading directory ${directoryPath}`);
@@ -52,10 +56,12 @@ export default class FileExplorerReloadPlugin extends Plugin {
             }
         }
 
-        for (const existingFileItem of existingFileItems) {
-            if (existingFileItem.isDirectory()) {
-                const path = this.combinePath(directoryPath, existingFileItem.name);
-                await this.reloadDirectory(path);
+        if (isRecursive) {
+            for (const existingFileItem of existingFileItems) {
+                if (existingFileItem.isDirectory()) {
+                    const path = this.combinePath(directoryPath, existingFileItem.name);
+                    await this.reloadDirectory(path, true);
+                }
             }
         }
     }
@@ -63,5 +69,25 @@ export default class FileExplorerReloadPlugin extends Plugin {
     private combinePath(directoryPath: string, fileName: string): string {
         const isRoot = directoryPath === ROOT_PATH;
         return isRoot ? fileName : `${directoryPath}/${fileName}`;
+    }
+
+    private handleFileMenu(menu: Menu, file: TAbstractFile) {
+        if (!(file instanceof TFolder)) {
+            return;
+        }
+
+        menu.addItem((item) => {
+            item
+                .setTitle("Reload Folder")
+                .setIcon("folder-sync")
+                .onClick(() => this.reloadDirectory(file.path, false));
+        });
+
+        menu.addItem((item) => {
+            item
+                .setTitle("Reload Folder with Subfolders")
+                .setIcon("folder-sync")
+                .onClick(() => this.reloadDirectory(file.path, true));
+        });
     }
 }

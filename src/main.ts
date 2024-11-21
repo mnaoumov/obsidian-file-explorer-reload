24c51c61bfafae8c6a87b812f59337637c40b5f8
Michael Naumov <mnaoumov@gmail.com>
2024-11-21 00:21:06 -0600

    Avoid default exports

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,4 @@
-export { default } from './FileExplorerReloadPlugin.ts';
+import { FileExplorerReloadPlugin } from './FileExplorerReloadPlugin.ts';
+
+// eslint-disable-next-line import-x/no-default-export
+export default FileExplorerReloadPlugin;

dd0991893b7c68abe999a99dde3197263705c5cd
Michael Naumov <mnaoumov@gmail.com>
2026-01-17 00:54:08 -0600

    refactor: commands

---
+++ src/Commands/ReloadFileExplorerCommand.ts
@@ -0,0 +1,29 @@
+import { CommandInvocationBase } from 'obsidian-dev-utils/obsidian/Commands/CommandBase';
+import { NonEditorCommandBase } from 'obsidian-dev-utils/obsidian/Commands/NonEditorCommandBase';
+
+import type { Plugin } from '../Plugin.ts';
+
+class ReloadFileExplorerCommandInvocation extends CommandInvocationBase<Plugin> {
+  public constructor(plugin: Plugin) {
+    super(plugin);
+  }
+
+  public override async execute(): Promise<void> {
+    await this.plugin.reloadFileExplorer();
+  }
+}
+
+export class ReloadFileExplorerCommand extends NonEditorCommandBase<Plugin> {
+  public constructor(plugin: Plugin) {
+    super({
+      icon: 'refresh-ccw',
+      id: 'reload-file-explorer',
+      name: 'Reload file explorer',
+      plugin
+    });
+  }
+
+  protected override createCommandInvocation(): CommandInvocationBase<Plugin> {
+    return new ReloadFileExplorerCommandInvocation(this.plugin);
+  }
+}

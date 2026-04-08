a38607dbb1684178acb93ade75fba8081b1a7779
Michael Naumov <mnaoumov@gmail.com>
2026-04-08 08:01:19 -0600

    chore: update template

--- src/Plugin.ts
+++ src/Plugin.ts
@@ -2,7 +2,7 @@ import {
   FileSystemAdapter,
   TFolder
 } from 'obsidian';
-import { PluginBase } from 'obsidian-dev-utils/obsidian/Plugin/PluginBase';
+import { PluginBase } from 'obsidian-dev-utils/obsidian/plugin/plugin-base';
 
 import type { PluginTypes } from './PluginTypes.ts';
 
